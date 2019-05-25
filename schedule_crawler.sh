a1832c778a85f62ed95a8515ff450f3873347389
yujunshuai <yujunshuai@iie.ac.cn>
2019-05-25 19:36:11 +0800

    update start script again

--- schedule_crawler.sh
+++ schedule_crawler.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-log_path=~/postGraduate/InformationRetrieve/IR_system/news-crawler-python/log/`date +%Y-%m-%d-%H-%M-%S`.log
-bin_path=~/postGraduate/InformationRetrieve/IR_system/news-crawler-python/news
+log_path=/root/ir/news-crawler-python/log/`date +%Y-%m-%d-%H-%M-%S`.log
+bin_path=/root/ir/news-crawler-python/news
 
 crawlerid=`ps aux | grep run-crawler.py | grep -v grep | awk '{print $2}'`
 if [ -n "$crawlerid" ];then
@@ -8,5 +8,5 @@ if [ -n "$crawlerid" ];then
 else
     rm -rf $bin_path/index2_files
     cd $bin_path
-    /Users/zhezhouli/anaconda3/envs/crawler/bin/python run-crawler.py >> $log_path  2>&1 &
+   /root/anaconda3/envs/ir/bin/python run-crawler.py >> $log_path  2>&1 &
 fi
